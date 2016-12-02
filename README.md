6e66244030fae406217058b8881726d3e293dfc4
Yibao Liu <734988964@qq.com>
2016-12-02 17:28:07 +0800

    push to github

---
+++ README.md
@@ -0,0 +1,20 @@
+### 中国地区数据
+####composer安装
+```
+composer require liuyibao/china-district
+```
+
+####使用方法
+```
+use Liuyibao\ChinaDistrict\Data;
+foreach(Data::fetchByThousand() as $thousandDistrict){
+    // todo
+    // save database
+    foreach($thousandDistrict as $district){
+        echo $district['id'], PHP_EOL;
+        echo $district['name'], PHP_EOL;
+        echo $district['level'], PHP_EOL;
+        echo $district['pid'], PHP_EOL;
+    }
+}
+```
---
+++ src/Data.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuyibao
+ * Date: 16-12-2
+ * Time: 下午4:55
+ */
+namespace Liuyibao\ChinaDistrict;
+class Data
+{
+    public static function fetchByThousand()
+    {
+        yield Data0001::$data;
+        yield Data0002::$data;
+        yield Data0003::$data;
+        yield Data0004::$data;
+        yield Data0005::$data;
+        yield Data0006::$data;
+        yield Data0007::$data;
+        yield Data0008::$data;
+        yield Data0009::$data;
+        yield Data0010::$data;
+        yield Data0011::$data;
+        yield Data0012::$data;
+        yield Data0013::$data;
+        yield Data0014::$data;
+        yield Data0015::$data;
+        yield Data0016::$data;
+        yield Data0017::$data;
+        yield Data0018::$data;
+        yield Data0019::$data;
+        yield Data0020::$data;
+        yield Data0021::$data;
+        yield Data0022::$data;
+        yield Data0023::$data;
+        yield Data0024::$data;
+        yield Data0025::$data;
+        yield Data0026::$data;
+        yield Data0027::$data;
+        yield Data0028::$data;
+        yield Data0029::$data;
+        yield Data0030::$data;
+        yield Data0031::$data;
+        yield Data0032::$data;
+        yield Data0033::$data;
+        yield Data0034::$data;
+        yield Data0035::$data;
+        yield Data0036::$data;
+        yield Data0037::$data;
+        yield Data0038::$data;
+        yield Data0039::$data;
+        yield Data0040::$data;
+        yield Data0041::$data;
+        yield Data0042::$data;
+        yield Data0043::$data;
+        yield Data0044::$data;
+        yield Data0045::$data;
+        yield Data0046::$data;
+    }
+}
